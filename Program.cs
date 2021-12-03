5234a89a856c2201414db3e9bc357017d18e41d4
diegosan1978 <dsantillan@outlook.com>
2021-12-03 15:57:28 -0300

    Nuevo proyecto

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            int edad;
+            DateTime fechaNacimiento;
+            double peso;
+
+            Console.WriteLine("Ingrese Nombre");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Ingrese Edad");
+            edad = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingrese FechaNacimiento");
+            fechaNacimiento = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Ingrese Peso");
+            peso = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+            Console.WriteLine("****************Datos Ingresados ****************");
+            Console.WriteLine("Nombre:" + nombre);
+            Console.WriteLine("Edad:" + edad);
+            Console.WriteLine("Nacimiento:" + fechaNacimiento);
+            Console.WriteLine("Peso:" + peso);
+            Console.ReadKey();
+
+        }
+    }
+}
